b546eeec2e4370feac42d3fe79aa0185b8ae4e8b
mernyeok <jwh1124@naver.com>
2021-01-28 14:46:13 +0900

    0.0.1

--- Test/lib/template.js
+++ Test/lib/template.js
@@ -1,1 +1,19 @@
-export.
+exports.HTML =function(title, list, body, control){
+    return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <a href="/author">author</a>
+      ${list}
+      ${control}
+      ${body}
+    </body>
+    </html>
+    `;
+}
+
